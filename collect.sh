6538a62cdb5b232e8a355aa3a49c1a560f2f6ddd
akry <a-yokokawa@axsh.net>
2014-09-03 15:33:26 +0900

    Fixed collect.sh

--- collect.sh
+++ collect.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-. ./files
+directory=$(cd $(dirname $0); pwd)
+files=(`ls ${directory}/config`)
 
 for file in ${files[@]}; do
 	[ ~/.${file} -nt ${file} ] && {
-		echo "copy ~/.${file} to ./${file}"
-		cp ~/.${file} ./${file}
+		echo "copy ~/.${file} to ${directory}/config/${file}"
+		cp ~/.${file} ${directory}/config/${file}
 	} || :
 done
