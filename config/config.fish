900d336e558fb49103633c71bc81d4bc0ce44a8b
akry <a-yokokawa@axsh.net>
2017-02-03 23:46:37 +0900

    Add config.fish and fix install script

---
+++ config/config.fish
@@ -0,0 +1,3 @@
+set -gx LSCOLORS gxfxcxdxbxegedabagacad
+set -gx CLICOLOR 1
+set -gx TERM xterm-256color
--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 directory=$(cd $(dirname $0); pwd)
-files=(`ls ${directory}/config`)
+conf_dir="${directory}/config"
 
-for file in ${files[@]}; do
-  if [ ~/.${file} -ot config/${file} -o ! -f ~/.${file} ]; then
-    echo "move config/${file} to ~/.${file}"
-    cp ./config/${file} ~/.${file}
-  fi
-done
+target_dir=~
+
+cp ${conf_dir}/emacs.el ${target_dir}/.emacs.el
+cp ${conf_dir}/config.fish ${target_dir}/.config/fish/config.fish
+cp ${conf_dir}/gitconfig ${target_dir}/.gitconfig
+cp ${conf_dir}/tmux.conf ${target_dir}/.tmux.conf
+cp ${conf_dir}/vimrc ${target_dir}/.vimrc
+cp ${conf_dir}/zprofile ${target_dir}/.zprofile
+cp ${conf_dir}/zshrc ~/.zshrc
